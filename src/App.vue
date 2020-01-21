dc564822807d619b7de5b7562e7fab54308cbe3b
Danny <windate3411@gmail.com>
2020-01-21 17:42:17 +0800

    finished project cloning

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
+    <div class="w-full max-w-md">
+      <h1 class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">A Draggable List</h1>
+      <draggable
+        tag="ul"
+        class="w-full max-w-md"
+        ghost-class="moving-card"
+        :list="users"
+        :animation="200"
+      >
+        <user-card
+          v-for="user in users"
+          :user="user"
+          :key="user.id"
+          @on-edit="onEdit"
+          @on-delete="onDelete"
+        ></user-card>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { EditIcon, Trash2Icon } from "vue-feather-icons";
+import Draggable from 'vuedraggable';
+import UserCard from './components/UserCard.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Adrian Schubert",
+          avatar: "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
+        },
+        {
+          id: 2,
+          name: "Violet Gates",
+          avatar: "https://pickaface.net/gallery/avatar/freud51c8b3f65e7dc.png"
+        },
+        {
+          id: 3,
+          name: "Steve Jobs",
+          avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
+        },
+        {
+          id: 4,
+          name: "Yassine Smith",
+          avatar: "https://pickaface.net/gallery/avatar/unr_yassine_191124_2012_3gngr.png"
+        },
+        {
+          id: 5,
+          name: "Senior Saez",
+          avatar: "https://pickaface.net/gallery/avatar/elmedinilla541c03412955c.png"
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Draggable,
+    UserCard
+  },
+  methods: {
+    onEdit(user) {
+      alert(`Editing ${user.name}`);
+    },
+    onDelete(user) {
+      alert(`Deleting ${user.name}`);
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.moving-card {
+  @apply opacity-50 bg-gray-100 border border-blue-500;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
